1d3b83786bd631bc339ca3ba65b2d53b619313d9
nerdeveloper <odirionye@gmail.com>
2021-03-02 19:37:00 +0100

    chore: Update GET

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/health', (req, res) => {
-  res.send('Healthy!!!')
+  res.send('Healthy!!!').status(200);
 })
 
+app.get('/', (req, res) => {
+  res.send('Welcome!!!')
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
