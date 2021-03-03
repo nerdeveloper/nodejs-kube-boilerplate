4ba35e7ad23b2db5bf2061098de6ef1f4ed6dbfe
nerdeveloper <odirionye@gmail.com>
2021-03-03 10:34:32 +0100

    chore: add endpoint

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const DB_USERNAME = process.env.MYSQL_USERNAME
+const DB_PASSWORD = process.env.MYSQL_PASSWORD
 
 app.get('/health', (req, res) => {
   res.send('Healthy!!!').status(200);
-})
+});
+
+app.get('/db_credentials', (req, res) => {
+  res.send({ db_username: DB_USERNAME, db_password: DB_PASSWORD })
+});
 
 app.get('/', (req, res) => {
   res.send('Welcome!!!')
